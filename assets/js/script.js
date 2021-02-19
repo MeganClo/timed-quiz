5b562e5e53588ae3dd89cd307c9736be76ceb0be
Megs <Megs.close@gmail.com>
2021-02-19 11:14:12 -0800

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+var QandA = [
+    {
+        question: "When a function belongs to an object, we refer to it as what?",
+        answersChoice: {
+            a: "a method",
+            b: "a principle",
+            c: "a property",
+            d: "a value"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Variables declared outside of any function are called what?",
+        answersChoice: {
+            a: "local function",
+            b: "useful function",
+            c: "global function", 
+            d: "single value"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which of the below is NOT a data type in JavaScript?",
+        answersChoice: {
+            a: "Boolean",
+            b: "String",
+            c: "null",
+            d: "all are data types in JavaScript",
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "We put our JavaScript inside what HTML element?",
+        answersChoice: {
+            a: "<jScript>",
+            b: "<sourceJs>",
+            c: "<script>",
+            d: "<href>"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "How would you put 'Hello Joe' in an alert box?",
+        answersChoice: {
+            a: "window.alert('Hello Joe')",
+            b: "alert.('Hello Joe')",
+            c: "message('Hello Joe')",
+            d: "both A and B are correct."
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How do you create a function called 'newFunction' in JavaScript?",
+        answersChoice: {
+            a: "newFunction()",
+            b: "function newFunction()",
+            c: "function = newFunction()",
+            d: "function (newFunction)"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is one way you can add a comment in JavaScript?",
+        answersChoice: {
+            a: "// comment would go here",
+            b: "<!-- comment would go here -->",
+            c: "'comment would go here'",
+            d: "* comment would go here"
+        },
+        correctAnswer: 'a'
+    },
+    
+]
